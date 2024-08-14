da48d20f4f0c474815f76c619aae85eb2e268851
salazangar <sidharthm673@gmail.com>
2024-08-14 13:24:55 +0530

    controller

--- premier-league/src/main/java/com/sid/premier_league/entity/Player.java
+++ premier-league/src/main/java/com/sid/premier_league/entity/Player.java
@@ -13,32 +13,46 @@ public class Player {
     @Column(name = "player_name", unique = true)
     private String name;
 
+    @Column(name = "nation")
     private String nation;
 
+    @Column(name = "position")
     private String pos;
 
+    @Column(name = "age")
     private Integer age;
 
+    @Column(name = "matches_played")
     private Integer mp;
 
+    @Column(name = "starts")
     private Integer starts;
 
+    @Column(name = "minutes_played")
     private Double min;
 
+    @Column(name = "goals")
     private Double gls;
 
+    @Column(name = "assists")
     private Double ast; //assists
 
+    @Column(name = "penalties_scored")
     private Double pk;
 
+    @Column(name = "yellow_cards")
     private Double crdy;
 
+    @Column(name = "red_cards")
     private Double crdr;
 
+    @Column(name = "expected_goals")
     private Double xg;
 
+    @Column(name = "expected_assists")
     private Double xag;
 
+    @Column(name = "team_name")
     private String team;
 
     public Player() {
